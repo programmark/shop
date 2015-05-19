f1e372668e7a6f0fa72243481f4b806447abd6de
mark <1127827675@qq.com>
2015-05-19 23:51:02 +0800

    up
    up

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
     include('init.php'); 
+    p(ocache::mumemcached()->set("ad", 1234));
     
     $smarty->display(PATH_VIEW . DS . 'show.html');
     
--- lib/functions.php
+++ lib/functions.php
@@ -28,6 +28,8 @@
             }
             return $data;
         }
+        
+        
 
     }
     
